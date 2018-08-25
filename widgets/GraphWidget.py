877e705d58114b73669194d4b1982aa5d499f13e
Maxime Faye <maxime.faye@gmail.com>
2018-08-25 09:51:55 +0200

    WIP realtime graph

--- widgets/GraphWidget.py
+++ widgets/GraphWidget.py
@@ -1,6 +1,6 @@
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
-from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 
 import matplotlib.pyplot as plt
 
@@ -8,18 +8,35 @@ import matplotlib.pyplot as plt
 class GraphWidget(BoxLayout):
 
     game_widget = None
+    to_display = []
+    graph_canvas = None
+    fig = None
+    ax = None
 
     def __init__(self, **kwargs):
         super(GraphWidget, self).__init__(**kwargs)
+        Clock.schedule_interval(self.update, 1.0 / 30.0)
         plt.ylabel('Scores')
         plt.xlabel('Transitions')
-        
-        Clock.schedule_interval(self.update, 1.0 / 60.0)
-        self.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        plt.ion()
+
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
+        self.line1, = self.ax.plot(self.to_display)
+        self.graph_canvas = FigureCanvasKivyAgg(figure=self.fig)
+
+        self.add_widget(self.graph_canvas)
 
     def update(self, dt):
-        if self.game_widget is not None:
-            print("Updated score length [{}]".format(len(self.game_widget.scores)))
-            plt.plot(self.game_widget.scores)
-            plt.draw()
+        if self.game_widget is None or len(self.game_widget.scores) == 0:
+            pass
+
+        nb_scores = len(self.game_widget.scores)
+        nb_points = min(nb_scores, 100)
+        start = nb_scores - nb_points
+        last_scores = self.game_widget.scores[start:nb_scores]
+        print(last_scores)
+        self.line1.set_ydata(last_scores)
+        self.graph_canvas.draw()
+        self.graph_canvas.flush_events()
 
