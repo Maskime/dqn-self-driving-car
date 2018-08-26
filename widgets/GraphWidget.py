b245c3c364578c886d6aaeb0b6261175a9357577
Maxime Faye <maxime.faye@gmail.com>
2018-08-26 06:52:32 +0200

    plot displaying correctly

--- widgets/GraphWidget.py
+++ widgets/GraphWidget.py
@@ -4,6 +4,8 @@ from kivy.uix.boxlayout import BoxLayout
 
 import matplotlib.pyplot as plt
 
+import numpy as np
+
 
 class GraphWidget(BoxLayout):
 
@@ -13,16 +15,17 @@ class GraphWidget(BoxLayout):
     fig = None
     ax = None
 
+    nb_pointsdisplay = 100
+
     def __init__(self, **kwargs):
         super(GraphWidget, self).__init__(**kwargs)
         Clock.schedule_interval(self.update, 1.0 / 30.0)
-        plt.ylabel('Scores')
-        plt.xlabel('Transitions')
-        plt.ion()
 
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111)
+        # self.ax.set_ylabel('Last Scores')
         self.line1, = self.ax.plot(self.to_display)
+        self.ax.set_xlim(0, self.nb_pointsdisplay)
         self.graph_canvas = FigureCanvasKivyAgg(figure=self.fig)
 
         self.add_widget(self.graph_canvas)
@@ -32,11 +35,14 @@ class GraphWidget(BoxLayout):
             pass
 
         nb_scores = len(self.game_widget.scores)
-        nb_points = min(nb_scores, 100)
+        nb_points = min(nb_scores, self.nb_pointsdisplay)
         start = nb_scores - nb_points
         last_scores = self.game_widget.scores[start:nb_scores]
-        print(last_scores)
+        x_data = range(0, len(last_scores))
+        # print(last_scores)
         self.line1.set_ydata(last_scores)
+        self.line1.set_xdata(x_data)
+        self.ax.set_ylim(np.min(last_scores), np.max(last_scores))
         self.graph_canvas.draw()
-        self.graph_canvas.flush_events()
+        # self.graph_canvas.flush_events()
 
